38ca19de0b2a2ef12e456782eb95252fa3bb3c75
Imran Abubakar <84298402+imran2975@users.noreply.github.com>
2023-03-08 22:53:20 +0100

    Create App.jsx

--- movie app/src/App.jsx
+++ movie app/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Movies from "./components/movies";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="./Screenshot 2023-03-07 at 17-14-20 Logo Maker Used By 2.3 Million Startups.png"
+              alt="Logo"
+              width="50"
+              height="50"
+              boarder-radius="50%"
+              className="d-inline-block align-text-center mr-1"
+            />
+            <span className="ml-1">Dev_Imran</span>
+          </a>
+        </div>
+      </nav>
+      <main className="container">
+        <Movies />
+      </main>
+    </React.Fragment>
   );
 }
 
