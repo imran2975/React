c90bc0f2f4b1ccd7fb50f3ea0890a5fe471937be
Imran Abubakar <84298402+imran2975@users.noreply.github.com>
2023-03-25 08:12:52 +0100

    Update movies.jsx

--- movie app/src/components/movies.jsx
+++ movie app/src/components/movies.jsx
@@ -73,7 +73,7 @@ class Movies extends Component {
 
     return (
       <div className="row">
-        <div className="col-3">
+        <div className="col-md-3 col-sm-12">
           <ListGroup
             items={this.state.genres}
             selectedItem={this.state.selectedGenre}
